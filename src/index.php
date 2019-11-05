f311e74a07db061ff31a8c048e4dabd0cbdf5c30
lurkshark <joel@dirty.vodka>
2019-11-04 23:31:42 -0800

    DBA file path override for Travis CI build

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@ in_array('lmdb', dba_handlers())
     or in_array('dba', dba_handlers())
     or exit('Neither lmdb or db4 implementations are available for dba!');
 $DBA_HANDLER = in_array('lmdb', dba_handlers()) ? 'lmdb' : 'db4';
-$DBA_PATH = 'cornchan.db';
+$DBA_PATH = $_ENV['CORN_DBA_PATH_OVERRIDE'] ?? './cornchan.db';
 
 // Utility functions
 function dba_replace_encode($key, $value, $handle) {
