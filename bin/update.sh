84b8db503392e796434840d37b0d84c53326ad9d
lurkshark <joel@dirty.vodka>
2019-11-05 00:53:38 -0800

    Add timestamping to wget update scripts

--- bin/update.sh
+++ bin/update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget --quiet --directory-prefix /home/public --input-file - << 'EOF'
+wget --quiet --timestamping --directory-prefix /home/public --input-file - << 'EOF'
 https://raw.githubusercontent.com/lurkshark/cornchan/master/src/.htaccess
 https://raw.githubusercontent.com/lurkshark/cornchan/master/src/index.php
 EOF
