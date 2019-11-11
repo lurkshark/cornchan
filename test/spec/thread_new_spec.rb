90d166f17db7be5368d45c0480bb9c40b595bdf0
joel <13933101+lurkshark@users.noreply.github.com>
2019-11-11 00:29:34 -0800

    Add basic replies implementation (#27)

--- test/spec/thread_new_spec.rb
+++ test/spec/thread_new_spec.rb
@@ -1,22 +1,22 @@
-feature "Posting a new thread to a board" do
+feature "Posting a new reply to a thread" do
   given(:subject) { Array.new(5) { CORPUS.sample }.join(" ") }
   given(:message) { Array.new(64) { CORPUS.sample }.join(" ") }
 
   background do
-    visit "/corn/new"
+    visit "/corn/10001/new"
   end
 
   context "when the form is fully filled-out" do
     background do
-      within("#newthread") do
+      within("#newreply") do
         fill_in "lorem", with: subject
         fill_in "ipsum", with: message
         click_button "Submit"
       end
     end
 
-    scenario "redirects to the board and shows the new thread" do
-      expect(page).to have_current_path("/corn/")
+    scenario "redirects to the thread and shows the new reply" do
+      expect(page).to have_current_path("/corn/10001")
       expect(page).to have_content(subject)
       expect(page).to have_content(message)
     end
@@ -24,41 +24,41 @@ feature "Posting a new thread to a board" do
 
   context "when the form is empty" do
     background do
-      within("#newthread") do
+      within("#newreply") do
         click_button "Submit"
       end
     end
 
     scenario "stays on the new thread page and shows an error" do
-      expect(page).to have_current_path("/corn/new")
-      expect(page).to have_content("You need a subject")
+      expect(page).to have_current_path("/corn/10001/new")
+      expect(page).to have_content("You need a subject or message")
     end
   end
 
   context "when the subject is empty" do
     background do
-      within("#newthread") do
+      within("#newreply") do
         fill_in "ipsum", with: message
         click_button "Submit"
       end
     end
 
-    scenario "stays on the new thread page and shows an error" do
-      expect(page).to have_current_path("/corn/new")
-      expect(page).to have_content("You need a subject")
+    scenario "redirects to the thread and shows the new message-only post" do
+      expect(page).to have_current_path("/corn/10001")
+      expect(page).to have_content(message)
     end
   end
 
   context "when the message is empty" do
     background do
-      within("#newthread") do
+      within("#newreply") do
         fill_in "lorem", with: subject
         click_button "Submit"
       end
     end
 
-    scenario "redirects to the board and shows the new subject-only post" do
-      expect(page).to have_current_path("/corn/")
+    scenario "redirects to the thread and shows the new subject-only post" do
+      expect(page).to have_current_path("/corn/10001")
       expect(page).to have_content(subject)
     end
   end
--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -13,3 +13,18 @@ Capybara.register_driver :selenium do |app|
     desired_capabilities: :firefox,
     url: "http://#{ENV['SELENIUM_HOST']}:#{ENV['SELENIUM_PORT']}/wd/hub")
 end
+
+CORPUS = ["lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", "sed", "do", "eiusmod",
+  "tempor", "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua", "adipiscing", "diam", "donec",
+  "adipiscing", "tristique", "risus", "nec", "feugiat", "in", "eu", "ultrices", "vitae", "auctor", "eu", "augue",
+  "ut", "lectus", "arcu", "bibendum", "odio", "euismod", "lacinia", "at", "quis", "risus", "sed", "vulputate", "ut",
+  "faucibus", "pulvinar", "elementum", "integer", "enim", "neque", "volutpat", "ac", "tincidunt", "aliquet",
+  "bibendum", "enim", "facilisis", "gravida", "neque", "convallis", "a", "cras", "vulputate", "mi", "sit", "amet",
+  "mauris", "commodo", "quis", "pharetra", "vel", "turpis", "nunc", "eget", "lorem", "dolor", "lacus", "sed",
+  "viverra", "tellus", "in", "hac", "habitasse", "platea", "dictumst", "tempor", "orci", "dapibus", "ultrices",
+  "in", "iaculis", "nunc", "sed", "enim", "facilisis", "gravida", "neque", "convallis", "a", "cras", "leo", "urna",
+  "magnis", "at", "elementum", "eu", "facilisis", "sed", "odio", "morbi", "in", "hac", "habitasse", "platea",
+  "dictumst", "quisque", "sagittis", "purus", "sit", "turpis", "nunc", "eget", "lorem", "dolor", "sed", "viverra",
+  "amet", "consectetur", "adipiscing", "elit", "pellentesque", "habitant", "morbi", "sapien", "et", "ligula",
+  "ullamcorper", "malesuada", "proin", "libero", "nunc", "consequat", "interdum", "elit", "eget", "gravida",
+  "cum", "sociis", "natoque", "penatibus", "et"]
