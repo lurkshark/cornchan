8f01d83b1fed827a70d1f843b549c17a64f94535
joel <13933101+lurkshark@users.noreply.github.com>
2019-11-15 12:40:12 -0800

    Specify /corn/ link more narrowly in spec

--- test/spec/00_root_spec.rb
+++ test/spec/00_root_spec.rb
@@ -1,8 +1,8 @@
 feature "Visiting the root page" do
   scenario "lists links to the boards" do
     visit "/"
-    expect(page).to have_link("corn")
-    expect(page).to have_link("prog")
+    expect(page).to have_link("/corn/")
+    expect(page).to have_link("/news/")
 
     click_link "corn"
     expect(page).to have_current_path("/corn/")
