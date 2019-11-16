47c402f1258f0360b710b66f708ac22d801a4d45
joel <13933101+lurkshark@users.noreply.github.com>
2019-11-16 15:36:49 -0800

    Refactor tests (#40)

--- bin/update.sh
+++ bin/update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# This script is used to keep the deployment up-to-date
 wget --quiet --timestamping --directory-prefix /home/public --input-file - << 'EOF'
 https://raw.githubusercontent.com/lurkshark/cornchan/master/src/.htaccess
 https://raw.githubusercontent.com/lurkshark/cornchan/master/src/index.php
@@ -6,9 +7,9 @@ EOF
 
 wget --quiet --timestamping --directory-prefix /home/public/static --input-file - << 'EOF'
 https://raw.githubusercontent.com/lurkshark/cornchan/master/src/static/.htaccess
+https://raw.githubusercontent.com/lurkshark/cornchan/master/src/static/favicon.png
 https://raw.githubusercontent.com/lurkshark/cornchan/master/src/static/normalize.css
 https://raw.githubusercontent.com/lurkshark/cornchan/master/src/static/style.css
-https://raw.githubusercontent.com/lurkshark/cornchan/master/src/static/favicon.png
 EOF
 
 # Nuke DB while still testing
